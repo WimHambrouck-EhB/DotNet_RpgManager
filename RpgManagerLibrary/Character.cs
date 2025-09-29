15d4b303e3c819e326843954c59f077d783f353c
WimHambrouck-EhB <wim.hambrouck@ehb.be>
2025-09-29 11:47:54 +0200

    Add project files.

---
+++ RpgManagerLibrary/Character.cs
@@ -0,0 +1,15 @@
+﻿namespace RpgManagerLibrary
+{
+    public class Character
+    {
+        public string Name { get; set; } = string.Empty;
+        public int Health { get; private set; }
+        public DateTime CreationDate { get; set; }
+        public int PowerLevel { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} (Health: {Health}, PowerLevel: {PowerLevel}, Created: {CreationDate})";
+        }
+    }
+}
